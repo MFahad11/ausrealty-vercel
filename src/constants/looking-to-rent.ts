ed47ab7274d4d01a5209b94357c89945970256cc
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2024-12-17 15:37:33 +0500

    Create looking-to-rent.ts

---
+++ src/constants/looking-to-rent.ts
@@ -0,0 +1,17 @@
+export const LOOKING_TO_RENT = [
+    {
+        image: "https://ausrealty-next.s3.ap-southeast-2.amazonaws.com/uploads/1bf79a3e-9e36-4004-adf8-5f11eb944355-SYLVANIA.jpg",
+    },
+    {
+        image: "https://ausrealty-next.s3.ap-southeast-2.amazonaws.com/uploads/de0420c1-68a7-4126-b2cd-a38fc89ac75e-SOUTH%20HURSTVILLE.jpg"
+    },
+    {
+        image: "https://ausrealty-next.s3.ap-southeast-2.amazonaws.com/uploads/a41d9ae6-7530-4601-b8fd-b283dbc58d47-RIVERWOOD.jpg"
+    },
+    {
+        image: "https://ausrealty-next.s3.ap-southeast-2.amazonaws.com/uploads/f9731fb5-2979-4950-a936-36fca7822384-PADSTOW.jpg"
+    },
+    {
+        image: "https://ausrealty-next.s3.ap-southeast-2.amazonaws.com/uploads/e7da975e-9888-4c9e-9b9d-a06d73876d3c-EDMONSONPARK-COMINGSOON.jpg"
+    }
+];

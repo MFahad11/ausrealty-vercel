f8622ac7c21946dfe956996eea7d0fbb583818e6
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2024-12-24 16:26:14 +0500

    Update [id].ts

---
+++ src/pages/api/domain/listings/[id].ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+  const dateUpdatedSince = new Date(
+    new Date().setFullYear(new Date().getFullYear() - 1)
+  )
+    .toISOString()
+    .split("T")[0];
+  const listingStatusFilter = "live";
+  try {
+    let params: any = {
+      dateUpdatedSince,
+      listingStatusFilter,
+    };
+    const response = await axios.get(
+      `https://api.domain.com.au/v1/listings/${id}`,
+      {
+        headers: {
+          accept: "application/json",
+          "X-Api-Key": process.env.DOMAIN_API_KEY,
+          "X-Api-Call-Source": "live-api-browser",
+        },
+        params: params,
+      }
+    );
+    res.status(200).json({
+      data: response.data,
+      success: true,
+    });
+  } catch (error: any) {
+    res.status(error.response?.status || 500).json({
+      success: false,
+      message: error.message || "Error fetching agency listings",
+    });
+  }
+}

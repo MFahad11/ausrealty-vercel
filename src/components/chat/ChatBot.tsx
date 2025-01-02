1eeb2a856dcb59a8988fa07ce8ac1a5a1481b467
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-02 17:02:16 +0500

    Update ChatBot.tsx

--- src/components/chat/ChatBot.tsx
+++ src/components/chat/ChatBot.tsx
@@ -704,7 +704,8 @@ const ChatBot = ({
                     <div className="text-left mb-2 p-3 space-x-1 flex items-center max-w-[80%] ">
         <div className="rounded-full h-3 w-3 bg-black animate-pulse"></div>
                     
-                    <p className="animate-pulse">
+                    <p className="animate-pulse text-[16px] font-light">
+                    
                       Getting the information for you
                     </p>
                     

f4ee3217d6d46048f0f059902612cc7f470d64b3
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-03 12:07:07 +0500

    Update ChatBot.tsx

--- src/components/chat/ChatBot.tsx
+++ src/components/chat/ChatBot.tsx
@@ -20,7 +20,7 @@ import {
   handleRenChat,
   handleSellingChat,
 } from "@/utils/openai";
-import { LuChevronDown, LuRotateCcw } from "react-icons/lu";
+import { LuChevronDown, LuLoader2, LuRotateCcw } from "react-icons/lu";
 import EmblaCarousel from "../ui/carousel";
 const ChatBot = ({
   title,
@@ -606,6 +606,17 @@ const ChatBot = ({
   return (
     <div className="w-full h-full flex flex-col justify-between">
       <ToastContainer />
+      {
+        (indexPage && intentExtracting) && (<div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+          <div className="bg-white px-8 py-6 rounded-lg shadow-lg flex flex-col items-center gap-4">
+            <LuLoader2 className="h-6 w-6 animate-spin text-black" />
+            <p className="text-black ">
+              Just a moment, we&apos;re working on your request
+            </p>
+          </div>
+        </div>)
+      }
+      
       <div className="max-w-4xl w-full mx-auto flex flex-col flex-grow pb-14 overflow-y-auto">
         <div className=" m-0 w-full rounded-lg mt-4">
           {title === "MOMENTS FROM HOME" && <InstaGrid data={instaData} />}

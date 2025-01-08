f439e7a7690fa84da3176d6ca8bf9d7d87b7db02
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-09 00:40:51 +0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import '@/components/ui/carousel/css/carousel.css';
+import '@/components/ui/carousel/css/agent-carousel.css';
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 

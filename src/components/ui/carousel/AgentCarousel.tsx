1059e5820aebe17741c845a499ca4ea5f2da4d0a
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-09 17:31:26 +0500

    Update AgentCarousel.tsx

--- src/components/ui/carousel/AgentCarousel.tsx
+++ src/components/ui/carousel/AgentCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { EmblaOptionsType } from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
 import AgentCard from '@/components/property/AgentCard'
@@ -6,20 +6,27 @@ type PropType = {
   agents: any[]
     children?: React.ReactNode
   options?: EmblaOptionsType
+  childEmblaRef?: any
+  stopPropagation?: (e: React.TouchEvent | React.MouseEvent) => void
+
 }
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-    const { options } = props
+    const { options,childEmblaRef} = props;
   const [selectedIndex, setSelectedIndex] = useState(0)
-  const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options)
-  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+
+  const [emblaMainRef, emblaMainApi] = useEmblaCarousel({
+    ...options,
     containScroll: 'keepSnaps',
-    dragFree: true
-  })
+    watchDrag: true, // Prevents parent from responding to drag during child interaction
+  });
+
 
   return (
     <div className='agent'><div className="embla">
-      <div className="embla__viewport"  ref={emblaMainRef}>
+      <div className="embla__viewport"
+      ref={childEmblaRef?childEmblaRef:emblaMainRef}
+      >
         <div className="embla__container">
             {
                 props?.children ? props.children : (
@@ -30,7 +37,7 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
                             <AgentCard agent={agent} 
                                         index={index}
                                         showLinks={false}
-                                      
+                                        
                                       />
                                       </div>
                         </div>

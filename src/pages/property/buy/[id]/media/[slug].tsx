55f7aea7bfc3742da567fb9329b34395286f6120
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-21 09:08:59 +0500

    add functionality for five step process

--- src/pages/property/buy/[id]/media/[slug].tsx
+++ src/pages/property/buy/[id]/media/[slug].tsx
@@ -150,7 +150,7 @@ export default function ImageGallery({ id,
   }
   return (
     <>
-    <Head>
+    {/* <Head>
         <title>{initialPropertyData?.addressParts?.displayAddress || `Property ${id}`}</title>
         <meta name="description" content={initialPropertyData.description || `Details for property ${id}`} />
         <link rel="canonical" href={canonicalUrl} />
@@ -163,7 +163,23 @@ export default function ImageGallery({ id,
         <meta name="twitter:title" content={initialPropertyData?.addressParts?.displayAddress || `Property ${id}`} />
         <meta name="twitter:description" content={initialPropertyData.description || `Details for property ${id}`} />
         <meta name="twitter:image" content={imageUrl} />
-      </Head>
+      </Head> */}
+
+<Head>
+         <title>{`Ausrealty`}</title>
+           <meta name="description" content={`Details for property`} />
+           <link rel="canonical" href={'https://devausrealty.vercel.app/'} />
+           <meta property="og:title" content={`Property`} />
+           <meta property="og:description" content={`Details for property`} />
+           <meta property="og:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+           <meta property="og:url" content={'https://devausrealty.vercel.app/'} />
+           <meta property="og:type" content="website" />
+           <meta name="twitter:card" content="summary_large_image" />
+           <meta name="twitter:title" content={`Property`} />
+           <meta name="twitter:description" content={`Details for property`} />
+           <meta name="twitter:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+         </Head>
     
       <NavBar backgroundColor="black" showBackButton={true} 
       backButtonLink={`/chat/looking-to-buy`}

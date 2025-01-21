fdaaa97b3ea568ff204e5dc54c2cd9664c9c4b0b
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-21 09:02:10 +0500

    add functionality for five step process

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <title>{`Ausrealty`}</title>
+        <meta name="description" content={`Details for property`} />
+        <link rel="canonical" href={'https://devausrealty.vercel.app/'} />
+        <meta property="og:title" content={`Property`} />
+        <meta property="og:description" content={`Details for property`} />
+        <meta property="og:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+        <meta property="og:url" content={'https://devausrealty.vercel.app/'} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`Property`} />
+        <meta name="twitter:description" content={`Details for property`} />
+        <meta name="twitter:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <body className="antialiased">

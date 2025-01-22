f598818ceb226c946062ce7e9eedb3d48a3f2c54
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-22 20:28:38 +0500

    preview link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+         <title>{`Ausrealty`}</title>
+         <meta name="twitter:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+         <meta name="twitter:card" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+         <meta name="twitter:title" content={`Ausrealty`} />
+         <meta name="twitter:description" content='Find your dream home with Ausrealty' />
+         <meta name="description" content='Find your dream home with Ausrealty' />
+         <meta property="og:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+         <meta property="og:site_name" content="Ausrealty"></meta>
+         <meta property="og:title" content={`Ausrealty`} />
+         <meta property="og:description" content='Find your dream home with Ausrealty' />
+         <meta property="og:url" content={'https://devausrealty.vercel.app/'} />
+         <link rel="canonical" href={'https://devausrealty.vercel.app/'} />
+         <meta property="description" content="Find your dream home with Ausrealty" />
+         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+         <meta property="og:type" content="website" />
+         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <body className="antialiased">
         <Main />
--- src/pages/property/buy/[id]/media/[slug].tsx
+++ src/pages/property/buy/[id]/media/[slug].tsx
@@ -169,34 +169,25 @@ export default function ImageGallery({ id,
   }
   return (
     <>
-    {/* <Head>
-        <title>{initialPropertyData?.addressParts?.displayAddress || `Property ${id}`}</title>
-        <meta name="description" content={initialPropertyData.description || `Details for property ${id}`} />
-        <link rel="canonical" href={canonicalUrl} />
-        <meta property="og:title" content={initialPropertyData?.addressParts?.displayAddress || `Property ${id}`} />
-        <meta property="og:description" content={initialPropertyData.description || `Details for property ${id}`} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content={canonicalUrl} />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={initialPropertyData?.addressParts?.displayAddress || `Property ${id}`} />
-        <meta name="twitter:description" content={initialPropertyData.description || `Details for property ${id}`} />
-        <meta name="twitter:image" content={imageUrl} />
-      </Head> */}
-
-<Head>
+    <Head>
          <title>{`Ausrealty`}</title>
-           <meta name="description" content={`Details for property`} />
-           <link rel="canonical" href={'https://devausrealty.vercel.app/'} />
-           <meta property="og:title" content={`Property`} />
-           <meta property="og:description" content={`Details for property`} />
-           <meta property="og:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
-           <meta property="og:url" content={'https://devausrealty.vercel.app/'} />
+         <meta name="twitter:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+         <meta name="twitter:card" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+         <meta name="twitter:title" content={`Ausrealty`} />
+         <meta name="twitter:description" content='Find your dream home with Ausrealty' />
+         <meta name="description" content='Find your dream home with Ausrealty' />
+         <meta property="og:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+         <meta property="og:site_name" content="Ausrealty"></meta>
+         <meta property="og:title" content={`Ausrealty`} />
+         <meta property="og:description" content='Find your dream home with Ausrealty' />
+         <meta property="og:url" content={'https://devausrealty.vercel.app/'} />
+         <link rel="canonical" href={'https://devausrealty.vercel.app/'} />
+         <meta property="description" content="Find your dream home with Ausrealty" />
+           
+           
            <meta property="og:type" content="website" />
-           <meta name="twitter:card" content="summary_large_image" />
-           <meta name="twitter:title" content={`Property`} />
-           <meta name="twitter:description" content={`Details for property`} />
-           <meta name="twitter:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
+           
+           
            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
          </Head>
     

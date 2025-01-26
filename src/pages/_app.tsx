02459cdd52c701f30edeb89d65bc751359ed1ed5
MFahad11 <fahadahad567@gmail.com>
2025-01-27 01:03:37 +0500

    Added new chart component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,16 +9,48 @@ import { useJsApiLoader } from "@react-google-maps/api";
 import ProgressLoader from "@/components/ui/ProgressLoader";
 import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 export default function App({ Component, pageProps }: AppProps) {
+  const GA_TRACKING_ID = "G-HJ4Y2HZ69J";
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_KEY || '', // Ensure this is correctly set
     libraries:["places"], // Corrected libraries
     version: "weekly",
   });
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url:string) => {
+      // @ts-ignore
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   if (loadError) return <div></div>;
   if (!isLoaded) return <ProgressLoader/>;
   return <>
-  
+  <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
   <DefaultSeo {...SEO} />
   <ToastContainer/>
 

88e0ea17f4f3dbadd1e97af98fe766dd4ca7a1bd
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-01-29 15:44:31 +0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-         <title>{`Ausrealty`}</title>
+         {/* <title>{`Ausrealty`}</title>
          <meta name="twitter:image" content={'https://beleef-public-uploads.s3.ap-southeast-2.amazonaws.com/pictures/preview.jpg'} />
          <meta name="twitter:card" content={'Find your dream home with Ausrealty'} />
          <meta name="twitter:title" content={`Ausrealty`} />
@@ -18,7 +18,7 @@ export default function Document() {
          <link rel="canonical" href={'https://devausrealty.vercel.app/'} />
          <meta property="description" content="Find your dream home with Ausrealty" />
          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-         <meta property="og:type" content="website" />
+         <meta property="og:type" content="website" /> */}
          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <body className="antialiased">

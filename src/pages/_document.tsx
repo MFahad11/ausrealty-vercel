3ca610c72631551fa018ae5b401b76c0363e2c89
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2025-02-06 12:17:41 +0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+           <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
+           <link rel="icon" type="image/png" sizes="180x180" href="/favicon-180x180.png" />
+           <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" />
            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" 
            key={'viewport'}
            />
